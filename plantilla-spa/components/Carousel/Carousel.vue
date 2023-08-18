1a415ecab0d3fc3da89c03a91c83431b0155f4e9
Franky Melero <77981015+frankymelero@users.noreply.github.com>
2023-08-18 14:55:45 +0200

    Avance diario

--- plantilla-spa/components/Carousel/Carousel.vue
+++ plantilla-spa/components/Carousel/Carousel.vue
@@ -1,4 +1,3 @@
-
 <template>
     <div class="container-fluid p-0 mb-5 pb-5">
       <div id="header-carousel" class="carousel slide carousel-fade" data-ride="carousel">
@@ -12,30 +11,73 @@
           ></li>
         </ol>
         <div class="carousel-inner">
-                <div class="carousel-item position-relative active" style="min-height: 100vh;">
-                    <img class="position-absolute w-100 h-100" src="../../static/assets/img/carousel-1.jpg" style="object-fit: cover;">
-                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                        <div class="p-3" style="max-width: 900px;">
-                            <h6 class="text-white text-uppercase mb-3 animate__animated animate__fadeInDown" style="letter-spacing: 3px;">Spa & Beauty Center</h6>
-                            <h3 class="display-3 text-capitalize text-white mb-3">Massage Treatment</h3>
-                            <p class="mx-md-5 px-5">Lorem rebum magna dolore amet lorem eirmod magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum labore diam</p>
-                            <a class="btn btn-outline-light py-3 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Make Appointment</a>
-                        </div>
-                    </div>
-                </div>
-         
+          <div class="carousel-item position-relative active" style="min-height: 100vh;">
+            <img class="position-absolute w-100 h-100" id="carouselimg" :src="activeCarouselItem.urlimg" style="object-fit: cover;">
+            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+              <div class="p-3" style="max-width: 900px;">
+                <h6 id="h1-message" class="text-white text-uppercase mb-3 animate__animated animate__fadeInDown">SPA & BEAUTY CENTER</h6>
+                <h3 class="display-3 text-capitalize text-white mb-3">{{ activeCarouselItem.h1 }}</h3>
+                <p id="description" class="mx-md-5 px-5">{{ activeCarouselItem.description }}</p>
+                <a class="btn btn-outline-light py-3 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Make Appointment</a>
+              </div>
             </div>
+          </div>
+        </div>
       </div>
     </div>
   </template>
   
   <script setup>
-  import { ref } from 'vue';
+  import { ref, computed, onMounted } from 'vue';
   
-  const carouselIndicators = ['element-1', 'element-2', 'element-3'];
+  const carouselIndicators = [
+    {
+      "id": "1",
+      "h1": "Massage Treatment",
+      "description": "Lorem rebum magna dolore amet lorem eirmod magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum labore diam",
+      "urlimg": "/_nuxt/static/assets/img/carousel-1.jpg"
+    },
+    {
+      "id": "2",
+      "h1": "Facial Treatment",
+      "description": "Lorem rebum magna dolore amet lorem eirmod magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum labore diam",
+      "urlimg": "/_nuxt/static/assets/img/carousel-2.jpg"
+    },
+    {
+      "id": "3",
+      "h1": "Cellulite Treatment",
+      "description": "Lorem rebum magna dolore amet lorem eirmod magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum labore diam",
+      "urlimg": "/_nuxt/static/assets/img/carousel-3.jpg"
+    }
+  ];
+  
   const activeIndex = ref(0);
   
+  //Crea la reactividad modificando el contenido en funcion del indice activo 
+  const activeCarouselItem = computed(() => {
+    return carouselIndicators[activeIndex.value];
+  });
+  
+  //Captura el indice del click, y setea el indice activo.
   const handleliClick = (index) => {
     activeIndex.value = index;
   };
+  
+  // Cambia automáticamente el índice activo cada 4 segundos
+  onMounted(() => {
+    const interval = setInterval(() => {
+      if (activeIndex.value === 2) {
+        activeIndex.value = 0;
+      } else {
+        activeIndex.value += 1;
+      }
+    }, 8000);
+  
+    // Limpia el intervalo cuando el componente se desmonta
+    onUnmounted(() => {
+      clearInterval(interval);
+    });
+
+  });
   </script>
+  
--- plantilla-spa/components/Testimonial/testimonial.vue
+++ plantilla-spa/components/Testimonial/testimonial.vue
@@ -9,7 +9,11 @@
             <h6 class="d-inline-block text-primary text-uppercase bg-light py-1 px-2">Testimonial</h6>
             <h1 class="mb-4">What Our Clients Say!</h1>
             <div class="owl-carousel testimonial-carousel">
-              <div v-for="(testimonial, index) in testimonials" :key="index" class="position-relative">
+              <div
+                v-for="(testimonial, index) in testimonials"
+                :key="index"
+                class="position-relative"
+                v-if="index === activeIndex">
                 <font-awesome-icon :icon="icons.quoteRight" class="fa-3x text-primary position-absolute" style="top: -6px; right: 0;" />
                 <div class="d-flex align-items-center mb-3">
                   <img class="img-fluid rounded-circle" :src="testimonial.image" style="width: 60px; height: 60px;" :alt="testimonial.clientName">
@@ -21,6 +25,15 @@
                 <p class="m-0">{{ testimonial.comment }}</p>
               </div>
             </div>
+            <ol class="carousel-indicators">
+              <li
+                v-for="(indicator, index) in testimonials"
+                :key="index"
+                :data-target="'#testimonial-carousel'"
+                @click="handleliClick(index)"
+                :class="{ active: index === activeIndex }"
+              ></li>
+            </ol>
           </div>
         </div>
       </div>
@@ -28,7 +41,7 @@
   </template>
   
   <script setup>
-  import { ref, onMounted } from 'vue';
+  import { ref, onMounted, onUnmounted } from 'vue';
   import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
   import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
   
@@ -37,36 +50,47 @@
   };
   
   const testimonials = [
-    {
-      clientName: 'Client Name 1',
-      profession: 'Profession 1',
-      image: 'src/static/assets/img/testimonial-1.jpg',
-      comment: 'Aliquyam sed elitr elitr erat sed diam ipsum eirmod eos lorem nonumy. Tempor sea ipsum diam  sed clita dolore eos dolores magna erat dolore sed stet justo et dolor.',
-    },
-    {
-      clientName: 'Client Name 2',
-      profession: 'Profession 2',
-      image: '@/static/assets/img/testimonial-2.jpg',
-      comment: 'Aliquyam sed elitr elitr erat sed diam ipsum eirmod eos lorem nonumy. Tempor sea ipsum diam  sed clita dolore eos dolores magna erat dolore sed stet justo et dolor.',
-    },
-    {
-      clientName: 'Client Name 3',
-      profession: 'Profession 3',
-      image: '@/static/assets/img/testimonial-3.jpg',
-      comment: 'Aliquyam sed elitr elitr erat sed diam ipsum eirmod eos lorem nonumy. Tempor sea ipsum diam  sed clita dolore eos dolores magna erat dolore sed stet justo et dolor.',
-    },
-    // Agrega más testimonios aquí...
-  ];
+  {
+    id: '1',
+    clientName: 'Client Name 1',
+    profession: 'Profession 1',
+    image: '/assets/img/testimonial-1.jpg',
+    comment: 'Aliquyam sed elitr elitr erat sed diam ipsum eirmod eos lorem nonumy. Tempor sea ipsum diam  sed clita dolore eos dolores magna erat dolore sed stet justo et dolor.',
+  },
+  {
+    id: '2',
+    clientName: 'Client Name 2',
+    profession: 'Profession 2',
+    image: '/assets/img/testimonial-2.jpg',
+    comment: 'Aliquyam sed elitr elitr erat sed diam ipsum eirmod eos lorem nonumy. Tempor sea ipsum diam  sed clita dolore eos dolores magna erat dolore sed stet justo et dolor.',
+  },
+  {
+    id: '3',
+    clientName: 'Client Name 3',
+    profession: 'Profession 3',
+    image: '/assets/img/testimonial-3.jpg',
+    comment: 'Aliquyam sed elitr elitr erat sed diam ipsum eirmod eos lorem nonumy. Tempor sea ipsum diam  sed clita dolore eos dolores magna erat dolore sed stet justo et dolor.',
+  },
+];
   
-  const firstCounter = ref(0);
-  const secondCounter = ref(0);
+  const activeIndex = ref(0);
   
-  const animateCounters = () => {
-    // ...
+  const handleliClick = (index) => {
+    activeIndex.value = index;
   };
   
+  // Intervalo para cambiar automáticamente el índice activo cada 4 segundos
   onMounted(() => {
-    animateCounters();
+    const interval = setInterval(() => {
+      if (activeIndex.value === testimonials.length - 1) {
+        activeIndex.value = 0;
+      } else {
+        activeIndex.value += 1;
+      }
+    }, 4000);
+  
+    onUnmounted(() => {
+      clearInterval(interval);
+    });
   });
   </script>
-  
