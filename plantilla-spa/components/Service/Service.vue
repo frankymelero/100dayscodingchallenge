efb9d61736dd8e30813026b76faaad839a17e840
Franky Melero <77981015+frankymelero@users.noreply.github.com>
2023-08-22 23:39:36 +0200

    Fix test 2 :D

--- plantilla-spa/components/Service/Service.vue
+++ plantilla-spa/components/Service/Service.vue
@@ -81,7 +81,7 @@ const services = [
   {
     title: 'Body Massage',
     description: 'Elitr labore sit dolor erat est lorem diam sea ipsum diam dolor duo sit ipsum',
-    image: '_nuxt/static/assets/img/service-1.jpg'
+    image: '../../static/assets/img/service-1.jpg'
   },
   {
     title: 'Stone Therapy',
